940e4e345e656a3f9c9bb63e3200a9560a971799
Weverson <negowfs@gmail.com>
2025-11-08 19:44:58 -0300

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+Product.delete_all
+
+product = Product.new(
+  title: 'Rails Scales!',
+  description: %(
+    <p>
+      <em>Practical Techniques for Performance and Growth</em><br>
+      Rails runs some of the biggest sites in the world, impacting millions of users
+      while efficiently crunching petabytes of data.
+      This book reveals how they do it, and how you can apply the same techniques
+      to your applications.
+    </p>
+  ),
+  price: 30.95
+)
+
+product.image.attach(
+  io: File.open(Rails.root.join('db', 'images', 'cprpo.jpg')),
+  filename: 'cprpo.jpg'
+)
+
+product.save!
