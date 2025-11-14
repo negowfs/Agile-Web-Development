e675790ab38fe203276eec62107d96f2fc3a2a9a
Weverson <negowfs@gmail.com>
2025-11-14 13:17:18 -0300

    interaction_g1

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,84 @@
+class OrdersController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: %i[new create]
+  before_action :ensure_cart_isnt_empty, only: %i[new]
+  before_action :set_order, only: %i[show edit update destroy]
+
+  # GET /orders
+  def index
+    @orders = Order.all
+  end
+
+  # GET /orders/1
+  def show
+  end
+
+  # GET /orders/new
+  def new
+    @order = Order.new
+  end
+
+  # GET /orders/1/edit
+  def edit
+  end
+
+  # POST /orders
+  def create
+    @order = Order.new(order_params)
+    @order.add_line_items_from_cart(@cart)
+
+    respond_to do |format|
+      if @order.save
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        format.html { redirect_to store_index_url, notice: "Thank you for your order." }
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /orders/1
+  def update
+    respond_to do |format|
+      if @order.update(order_params)
+        format.html { redirect_to @order, notice: "Order was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @order }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /orders/1
+  def destroy
+    @order.destroy
+
+    respond_to do |format|
+      format.html { redirect_to orders_path, notice: "Order was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    # Garante que o carrinho não está vazio
+    def ensure_cart_isnt_empty
+      if @cart.line_items.empty?
+        redirect_to store_index_url, notice: "Your cart is empty"
+      end
+    end
+
+    # Define @order para ações show, edit, update, destroy
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Strong parameters para permitir somente campos confiáveis
+    def order_params
+      params.require(:order).permit(:name, :address, :email, :pay_type)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root "store#index", as: "store_index"
+  root "store#index", as: :store_index
   resources :line_items
   resources :carts
   resources :products
+  resources :orders
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
