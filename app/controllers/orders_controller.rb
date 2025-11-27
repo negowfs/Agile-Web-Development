aa6ecfa7dc9a2f3989c689c3991603353af04bcb
Weverson <negowfs@gmail.com>
2025-11-27 18:13:19 -0300

    testes_interaction_h2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,7 +30,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OderMailer.received(@order).deliver_later
+        ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
         format.html { redirect_to store_index_url, notice: "Thank you for your order." }
         format.json { render :show, status: :created, location: @order }
       else
@@ -65,20 +65,29 @@ class OrdersController < ApplicationController
 
   private
 
-    # Garante que o carrinho não está vazio
-    def ensure_cart_isnt_empty
+      def ensure_cart_isnt_empty
       if @cart.line_items.empty?
         redirect_to store_index_url, notice: "Your cart is empty"
       end
     end
 
-    # Define @order para ações show, edit, update, destroy
     def set_order
       @order = Order.find(params[:id])
     end
 
-    # Strong parameters para permitir somente campos confiáveis
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
+    end
+
+    def pay_type_params
+      if order_params[:pay_type] == "Credit card"
+        params.require(:order).permit(:credit_card_number, :expiration_date)
+      elsif order_params[:pay_type] == "Check"
+        params.require(:order).permit(:routing_number, :account_number)
+      elsif order_params[:pay_type] == "Purchase order"
+        params.require(:order).permit(:po_num)
+      else
+        {}
+      end
     end
 end
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,5 @@ require "test_helper"
 require "selenium/webdriver"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
+  driven_by :selenium, using: :firefox, screen_size: [ 1400, 1400 ]
 end
