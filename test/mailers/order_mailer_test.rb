eeecd55b29c968beba7425f24e9652b25caa670d
Weverson <negowfs@gmail.com>
2025-11-28 20:17:15 -0300

    Try to fix errors

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -1,22 +1,22 @@
 require "test_helper"
 
-class OderMailerTest < ActionMailer::TestCase
+class OrderMailerTest < ActionMailer::TestCase
   test "received" do
-    mail = OderMailer.received(orders(:one))
+    mail = OrderMailer.received(orders(:one))
     assert_equal "Pragmatic Store Order Confirmation", mail.subject
     assert_equal [ "dave@example.org" ], mail.to
     assert_equal [ "depot@example.com" ], mail.from
-    assert_match /1 x Pragmatic Programmer/, mail.body.encoded
+    assert_match /1 x The Pragmatic Programmer/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OderMailer.shipped(orders(:one))
+    mail = OrderMailer.shipped(orders(:one))
     assert_equal "Pragmatic Store Order Shipped", mail.subject
     assert_equal [ "dave@example.org" ], mail.to
     assert_equal [ "depot@example.com" ], mail.from
     assert_match %r{
       <td[^>]*>1<\/td<\s*
-      <td>&time;<\/td>\s*
+      <td>&times;<\/td>\s*
       <td>[^>]*>\s*The\sPragmatic\sProgrammer\s*<\/td>
     }x, mail.body.encoded
   end
